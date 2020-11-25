6375bbcc6f9faf13bf941f02a52881fef82b17ea
Mathis Bullinger <mathis@bullinger.dev>
2020-11-25 13:24:09 +0100

    feat: read podcast info from ddb

--- src/resolvers/fields/podcast.ts
+++ src/resolvers/fields/podcast.ts
@@ -1,11 +1,14 @@
 import { query } from '~/utils/podcastIndex'
-import { numberToId } from '~/utils/id'
+import { numberToId, idToNumber } from '~/utils/id'
 
 export const episodes = async ({ id }) => {
-  const { items } = await query('episodes/byfeedid', { id, max: 200 })
+  const { items } = await query('episodes/byfeedid', {
+    id: typeof id === 'number' ? id : idToNumber(id),
+    max: 200,
+  })
   return items
 }
 
-export const id = ({ id }) => numberToId(id)
+export const id = ({ id }) => (typeof id === 'number' ? numberToId(id) : id)
 
-export const feed = ({ url }) => url
+export const feed = ({ url, feed }) => feed ?? url
--- src/resolvers/queries.ts
+++ src/resolvers/queries.ts
@@ -1,6 +1,7 @@
 import * as pi from '~/utils/podcastIndex'
 import axios from 'axios'
 import { idToNumber } from '~/utils/id'
+import { ddb } from '~/utils/aws'
 
 export const search = async (_, { query, limit }) => {
   const { feeds } = await pi.query('search/byterm', {
@@ -11,6 +12,10 @@ export const search = async (_, { query, limit }) => {
 }
 
 export const podcast = async (_, { id }) => {
+  const { Item } = await ddb
+    .get({ TableName: 'echo_main', Key: { pk: id, sk: 'meta' } })
+    .promise()
+  if (Item) return Item
   const { feed } = await pi.query('podcasts/byfeedid', { id: idToNumber(id) })
   return feed
 }
